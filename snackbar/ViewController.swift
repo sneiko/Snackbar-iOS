6039faf40536feaef661578fca0b694623f82d12
Сергей Нейкович <s_neiko@outlook.com>
2016-11-30 21:41:15 +0300

    Initial Commit

---
+++ snackbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snackbar
+//
+//  Created by Сергей on 30.11.16.
+//  Copyright © 2016 Fish in Sky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
